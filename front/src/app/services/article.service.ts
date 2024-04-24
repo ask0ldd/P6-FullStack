bed3ad74f69c7b1eff2f668d2ceda464d1e31ce8
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-24 02:24:22 +0200

    *edit* article service : created method added

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { IArticle } from '../interfaces/IArticle';
+import { INewArticleRequest } from '../interfaces/Requests/INewArticleRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,10 @@ export class ArticleService {
   }
 
   all$(): Observable<IArticle[]>{
-    // console.log("triggered")
-    // this.http.get<IArticle[]>('api/articles').subscribe(data => console.log(data))
     return this.http.get<IArticle[]>('api/articles')
+  }
+
+  create$(newArticle : INewArticleRequest) : Observable<any>{
+    return this.http.post<any>('api/article', newArticle)
   }
 }
