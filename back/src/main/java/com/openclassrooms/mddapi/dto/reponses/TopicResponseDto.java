5db4b638ed43813b80f75a0679a2ef2b550a902a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-27 12:04:52 +0200

    *add* back / topicresponse dto complete

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/reponses/TopicResponseDto.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.mddapi.dto.reponses;
+
+import com.openclassrooms.mddapi.models.Topic;
+import com.openclassrooms.mddapi.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopicResponseDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<User> users;
+
+    public TopicResponseDto(Topic topic) {
+        this.id = topic.getId();
+        this.name = topic.getName();
+        this.description = topic.getDescription();
+        this.createdAt = topic.getCreatedAt();
+        this.updatedAt = topic.getUpdatedAt();
+        this.users = topic.getUsers();
+    }
+
+}
