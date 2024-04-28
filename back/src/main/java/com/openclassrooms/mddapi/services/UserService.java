be7491a2ff0671146a6a092c404c333e3445bf3e
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-28 20:37:46 +0200

    *edit* back / userdetailsservice now implemented by userservice

--- back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/UserService.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.mddapi.services;
 
+import com.openclassrooms.mddapi.exceptions.UserNotFoundException;
 import com.openclassrooms.mddapi.models.User;
 import com.openclassrooms.mddapi.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -14,10 +18,16 @@ public class UserService {
     }
 
     public User getById(Long id){
-        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Target user cannot be found."));
     }
 
     public User create(User user){
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not valid."));
     }
 }
