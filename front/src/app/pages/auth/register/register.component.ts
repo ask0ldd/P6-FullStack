1ba5f75ddd9567172068c6c0e91d79adab0474f3
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-29 15:10:35 +0200

    *add* front / form register component

---
+++ front/src/app/pages/auth/register/register.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IRegisterRequest } from 'src/app/interfaces/Requests/IRegisterRequest';
+import { IJwtResponse } from 'src/app/interfaces/Responses/IJwtResponse';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  public registerForm : FormGroup = this.fb.group({
+    username : [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    ]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService) {
+}
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void{
+    const registerRequest = this.registerForm.value as IRegisterRequest;
+    this.authService.register$(registerRequest).subscribe((jwtResponse : IJwtResponse )=> {
+      localStorage.setItem('token', jwtResponse.token);
+    })
+  }
+
+}
