1b0e94777254f8d200d7834dc29db3baf3d5c167
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-30 18:28:02 +0200

    *add* security configuration

---
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,78 @@
+package com.openclassrooms.mddapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final JwtDecoder jwtDecoder;
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public SecurityConfiguration(JwtDecoder jwtDecoder, JwtAuthenticationConverter jwtAuthenticationConverter) {
+        this.jwtDecoder = jwtDecoder;
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        daoProvider.setUserDetailsService(userDetailsService);
+        daoProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoProvider);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // csrf disabled cause stateless
+        return http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                // .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            // all routes but login, register & swaggers needs to be authenticated
+                            .requestMatchers(new AntPathRequestMatcher("/api/articles/*")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/article/**")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/articles")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/topic/**")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/topics/**")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/topics")).hasAnyRole("USER", "ADMIN")
+                            .requestMatchers(new AntPathRequestMatcher("/api/auth/newcredentials")).hasAnyRole("USER", "ADMIN")
+                            // .requestMatchers(new AntPathRequestMatcher("/api/auth/newcredentials")).permitAll()
+                            .requestMatchers(WHITELIST).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(
+                        oauth2ResourceServer -> oauth2ResourceServer.jwt(jwt -> jwt.decoder(jwtDecoder)
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .build();
+    }
+
+    private static final String[] WHITELIST = {
+            "/api/auth/login",
+            "/api/auth/register",
+    };
+
+    // this will encode the user's password
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
