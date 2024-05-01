0e826b40534a0203b2715ae9fb4d6ad3677011e8
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-01 16:38:44 +0200

    *add* back / topic mapper

---
+++ back/src/main/java/com/openclassrooms/mddapi/mappers/TopicMapper.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.mddapi.mappers;
+
+public class TopicMapper {
+    /*private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static TopicDTO toDTO(Topic topic) {
+        TopicDTO topicDTO = modelMapper.map(topic, TopicDTO.class);
+        topicDTO.setUserIds(topic.getUsers().stream()
+                .map(User::getId)
+                .collect(Collectors.toList()));
+        return topicDTO;
+    }*/
+}
