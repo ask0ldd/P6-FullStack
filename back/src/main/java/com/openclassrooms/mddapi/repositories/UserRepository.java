c68ab801757bf574c3c0d894dea513efa52545b2
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-02 15:16:09 +0200

    *add* back / user repository

---
+++ back/src/main/java/com/openclassrooms/mddapi/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.mddapi.repositories;
+
+import com.openclassrooms.mddapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // custom query
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+}
