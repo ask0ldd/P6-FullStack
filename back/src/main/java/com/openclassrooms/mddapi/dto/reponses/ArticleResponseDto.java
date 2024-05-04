00e1f51ed029901e28358a39468609ea2c7d9fe5
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-04 14:29:11 +0200

    *edit* back / topicresponsedto now gets more selective datas

--- back/src/main/java/com/openclassrooms/mddapi/dto/reponses/ArticleResponseDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/reponses/ArticleResponseDto.java
@@ -1,9 +1,9 @@
 package com.openclassrooms.mddapi.dto.reponses;
 
+import com.openclassrooms.mddapi.dto.partials.LimitedUserDto;
 import com.openclassrooms.mddapi.models.Article;
 import com.openclassrooms.mddapi.models.Comment;
 import com.openclassrooms.mddapi.models.Topic;
-import com.openclassrooms.mddapi.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,9 +19,9 @@ public class ArticleResponseDto {
     private Long id;
     private String title;
     private String content;
-    private User user;
-    private List<Comment> comments;
-    private Topic topic;
+    private LimitedUserDto user;
+    private List<LimitedComment> comments;
+    private LimitedTopic topic;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
@@ -29,11 +29,43 @@ public class ArticleResponseDto {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
-        this.comments = article.getComments();
+        this.comments = article.getComments().stream().map(LimitedComment::new).toList();
         this.createdAt = article.getCreatedAt();
         this.updatedAt = article.getUpdatedAt();
-        this.user = article.getUser();
-        this.topic = article.getTopic();
+        this.user = new LimitedUserDto(article.getUser());
+        this.topic = new LimitedTopic(article.getTopic());
     }
 
+    @Data
+    @AllArgsConstructor
+    private static class LimitedComment{
+        private Long id;
+        private LimitedUserDto user;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+        public LimitedComment(Comment comment){
+            this.id = comment.getId();
+            this.user = new LimitedUserDto(comment.getUser());
+            this.content = comment.getContent();
+            this.createdAt = comment.getCreatedAt();
+            this.updatedAt = comment.getUpdatedAt();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class LimitedTopic{
+        private Long id;
+        private String name;
+        private String description;
+
+        public LimitedTopic(Topic topic){
+            this.id = topic.getId();
+            this.name = topic.getName();
+            this.description = topic.getDescription();
+        }
+    }
 }
+
