606d3fec7e3b7fe921b93b75c368960c5ed14002
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-05 16:16:12 +0200

    *edit* back / global exceptions handler : bad credentials, resource not found, number format, usernotfound added

--- back/src/main/java/com/openclassrooms/mddapi/exceptions/GlobalExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.openclassrooms.mddapi.dto.reponses.DefaultResponseDto;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,21 +13,50 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        return new ResponseEntity<>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<DefaultResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<DefaultResponseDto> handleAuthenticationExceptions(AuthenticationException ex) {
+        System.out.println("auth excep");
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto("Authentication failed."), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolationExceptions(ConstraintViolationException ex) {
-        return new ResponseEntity<>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<DefaultResponseDto> handleConstraintViolationExceptions(ConstraintViolationException ex) {
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ArticleNotFoundException.class)
-    public ResponseEntity<Object> handleConstraintViolationExceptions(ArticleNotFoundException ex) {
-        return new ResponseEntity<>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<DefaultResponseDto> handleResourceNotFoundExceptions(ResourceNotFoundException ex) {
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto(formatErrorMessage(ex.getMessage())), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<DefaultResponseDto> handleBadCredentialsExceptions(BadCredentialsException ex) {
+        System.out.println("bad excep");
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto("Bad credentials."), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<DefaultResponseDto> handleNumberFormatExceptions(NumberFormatException ex) {
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto("Can't parse the given String to Long."), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<DefaultResponseDto> handleUserNotFoundExceptions(UserNotFoundException ex) {
+        return new ResponseEntity<DefaultResponseDto>(new DefaultResponseDto("User not found."), HttpStatus.UNAUTHORIZED);
     }
 
     private String formatErrorMessage(String rawErrorMessage){
         return rawErrorMessage.substring(rawErrorMessage.lastIndexOf("[")+1).replace("]]", "").stripTrailing();
     }
 }
+
+/*
+A DisabledException must be thrown if an account is disabled and the AuthenticationManager can test for this state.
+A LockedException must be thrown if an account is locked and the AuthenticationManager can test for account locking.
+A BadCredentialsException must be thrown if incorrect credentials are presented. Whilst the above exceptions are optional, an AuthenticationManager must always test credentials.
+
+ */
