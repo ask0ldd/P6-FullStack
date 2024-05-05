fc4718150378a9730045167c86c6e8f16f7665fc
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-05 16:09:46 +0200

    *edit* improve some dto error messages

--- back/src/main/java/com/openclassrooms/mddapi/dto/payloads/NewArticlePayloadDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/payloads/NewArticlePayloadDto.java
@@ -7,14 +7,14 @@ import lombok.Data;
 @Data
 public class NewArticlePayloadDto {
 
-    @NotBlank
+    @NotBlank(message = "A topic needs to be selected")
     private String topicId;
 
     @NotBlank
-    @Size(min = 3, max = 60)
+    @Size(min = 3, max = 60, message = "Title length must be between 3 and 60")
     private String title;
 
     @NotBlank
-    @Size(min = 3, max = 400)
+    @Size(min = 3, max = 400, message = "Content length must be between 3 and 400")
     private String content;
 }
--- back/src/main/java/com/openclassrooms/mddapi/dto/payloads/RegisterPayloadDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/payloads/RegisterPayloadDto.java
@@ -8,17 +8,17 @@ import lombok.Data;
 
 @Data
 public class RegisterPayloadDto {
-    @NotBlank
-    @Size(min = 6, max = 20)
+    @NotBlank(message = "Username can't be blank.")
+    @Size(min = 6, max = 20, message = "Username length must be between 6 & 20.")
     private String username;
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
+    @NotBlank(message = "Email can't be blank.")
+    @Size(max = 50, message = "Email length can't exceed 50.")
+    @Email(message = "Not a valid email format.")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password can't be blank.")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$", message = "Password must be at least 8 characters long and contain at least one number, one lowercase letter, one uppercase letter, and one special character.")
-    @Size(min = 8, max = 40)
+    @Size(min = 8, max = 40, message = "Password length must be between 8 & 40.")
     private String password;
 }
