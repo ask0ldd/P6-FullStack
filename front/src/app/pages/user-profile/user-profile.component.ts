77b905ab058d4eedfb34f5b51dabc02d0492eb99
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-06 17:01:17 +0200

    *edit* front / user profile : added new credentials submission

--- front/src/app/pages/user-profile/user-profile.component.ts
+++ front/src/app/pages/user-profile/user-profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subscription, of, take } from 'rxjs';
 import { ITopic } from 'src/app/interfaces/ITopic';
+import { IUpdateCredentialsRequest } from 'src/app/interfaces/Requests/IUpdateCredentialsRequest';
+import { ICredentialsResponse } from 'src/app/interfaces/Responses/ICredentialsResponse';
 import { AuthService } from 'src/app/services/auth.service';
 import { TopicService } from 'src/app/services/topic.service';
 
@@ -14,19 +16,19 @@ import { TopicService } from 'src/app/services/topic.service';
 export class UserProfileComponent implements OnInit {
 
   userId : number = 1 // !!! should be retrieved through token
-  retrievedTopics! : ITopic[]
+  retrievedTopics : ITopic[] = []
   subscriptions : Subscription[] = []
 
   public updateCredentialsForm : FormGroup = this.fb.group({
     username: [
-      '',
+      "",
       [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(6)
       ]
     ],
     email: [
-      '',
+      "",
       [
         Validators.required,
         Validators.email
@@ -44,27 +46,39 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {
     this.refreshTopics()
+    this.getUsercredentials()
   }
 
   onCredentialsSubmit() : void {
-    // !!!!
+    const newCredentials = this.updateCredentialsForm.value as IUpdateCredentialsRequest
+    console.log(newCredentials)
+    this.authService.updateCredentials$(newCredentials).pipe(take(1)).subscribe({
+      next : _ => {
+        this.authService.flushStorage()
+        this.router.navigate(['login'])
+      },
+      error : error => console.log(error?.error)
+    })
   }
 
-  unsubscribeFromTopic(event: any) : void{
+  unsubscribeFromTopic(event: any) : void {
     this.topicService.unsubscribe$(event.topicId, this.userId).pipe(take(1)).subscribe(_ => this.refreshTopics())
   }
 
-  refreshTopics(): void{
+  refreshTopics(): void {
     this.subscriptions.forEach(sub => sub.unsubscribe())
     const sub = this.topicService.allTopicsAUserIsSubscribedTo$(this.userId).subscribe(datas => this.retrievedTopics = datas)
     this.subscriptions.push(sub)
   }
 
-  getUsercredentials() : void{
-    // !!!
+  getUsercredentials() : void {
+    this.authService.getCredentials$().pipe(take(1)).subscribe((datas : ICredentialsResponse) => {
+      this.updateCredentialsForm.get('email')!.setValue(datas.email)
+      this.updateCredentialsForm.get('username')!.setValue(datas.username)
+    })
   }
 
-  logout() : void{
+  logout() : void {
     this.authService.flushStorage()
     this.router.navigateByUrl('login')
   }
