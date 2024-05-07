9d75dbc6d10eb8ad5280eb7d727f04a5fcf86bd5
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-07 14:35:14 +0200

    *add* back / comment model

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Comment.java
@@ -0,0 +1,47 @@
+package com.openclassrooms.mddapi.models;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "comments")
+@Table(name = "comments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id", nullable = false)
+    // @JsonBackReference
+    // @JsonManagedReference
+    @JsonIgnore
+    private Article article;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonManagedReference // when comment is serialized, { comment: ... ,  user : {} } the user object will be added
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
