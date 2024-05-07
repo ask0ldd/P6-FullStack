af948a798dcfb8c254270412ecabac706a9d306d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-07 14:34:57 +0200

    *add* back / article model

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Article.java
@@ -0,0 +1,66 @@
+package com.openclassrooms.mddapi.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity(name = "articles")
+@Table(name = "articles")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    // mappedBy = "article" :
+    // In this case, the child entity has a property named "article" that maps to the parent entity.
+    // CascadeType.ALL
+    // means that all lifecycle operations (persist, remove, refresh, merge, and detach)
+    // performed on the parent entity will be cascaded to the associated child entities.
+    // orphanRemoval = true:
+    // This element indicates that when a child entity is removed from the collection in the parent entity,
+    // the child entity should also be removed from the database.
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Comment> comments = new ArrayList<>();
+
+    @ManyToOne // needed details
+    @JoinColumn(name = "topic_id", nullable = false)
+    @JsonBackReference
+    private Topic topic;
+
+    @ManyToOne // needed : author
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonManagedReference
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
