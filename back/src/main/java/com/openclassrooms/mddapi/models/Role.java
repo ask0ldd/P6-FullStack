d927316b7e37ed9ded0bcb3663618cce29dd5e15
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-07 14:34:32 +0200

    *add* back / role model

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Role.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.models;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@Entity(name = "roles")
+@Builder
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer roleId;
+
+    private String authority;
+
+    public Role() {
+        super();
+    }
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role(Integer roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    /*@Override
+    public String getAuthority() {
+        return this.authority;
+    }*/
+
+}
