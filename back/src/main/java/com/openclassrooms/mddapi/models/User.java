c388e4388621b51295f98e4d7df192a47daa21a7
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-07 14:35:45 +0200

    *add* back / user model

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/User.java
@@ -0,0 +1,118 @@
+package com.openclassrooms.mddapi.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email"),
+        @UniqueConstraint(columnNames = "username"),
+})
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"})
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Size(max = 50)
+    @Email
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NonNull
+    @Size(max = 20)
+    @Column(name = "username", unique = true)
+    private String name;
+
+    /*@OneToMany(mappedBy = "user", orphanRemoval = true)
+    // @JsonManagedReference
+    @JsonBackReference // when user is serialized, { user: ... ,  articles : [] } the article object won't be added anymore
+    private List<Article> articles;*/
+
+    /*@ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_topics_junction", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "topic_id") })
+    private Set<Topic> topics;*/
+
+    /*@ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_topics_junction",
+            joinColumns = @JoinColumn( name = "user_id" ),
+            inverseJoinColumns = @JoinColumn( name = "topic_id" ) )
+    private List<Topic> topics;*/
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles_junction", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+            @JoinColumn(name = "role_id") })
+    private Set<Role> authorities;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    @JsonIgnore
+    private List<Comment> comments;
+
+    @NonNull
+    @Size(max = 120)
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
