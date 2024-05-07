6fd449e061ac50b6354715b02f5105cea4c5ef40
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-07 14:35:29 +0200

    *add* back / topic model

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Topic.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.mddapi.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Entity(name = "topics")
+@Table(name = "topics")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @OneToMany(mappedBy = "topic")
+    @JsonManagedReference
+    private List<Article> articles;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_topics_junction",
+            joinColumns = @JoinColumn( name = "topic_id" ),
+            inverseJoinColumns = @JoinColumn( name = "user_id" ) )
+    private List<User> users;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
