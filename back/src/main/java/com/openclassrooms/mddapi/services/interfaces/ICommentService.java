47869e40c91a1aeec1455d3a273d1a0dee07c554
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-11 14:21:56 +0200

    *edit* back / article form displaying an error when the api endpoint return one

--- back/src/main/java/com/openclassrooms/mddapi/services/interfaces/ICommentService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/interfaces/ICommentService.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.mddapi.services.interfaces;
 
+import com.openclassrooms.mddapi.exceptions.ArticleNotFoundException;
+import com.openclassrooms.mddapi.exceptions.UserNotFoundException;
 import com.openclassrooms.mddapi.models.Comment;
 
+import java.security.Principal;
 import java.util.List;
 
 public interface ICommentService {
@@ -20,4 +23,16 @@ public interface ICommentService {
      * @return the created comment
      */
     Comment create(Comment comment);
+
+    /**
+     * Creates a new comment for a given article.
+     *
+     * @param principal   the authenticated principal (user) creating the comment
+     * @param articleId   the ID of the article to which the comment is being added
+     * @param commentBody the content of the comment
+     * @return the newly created comment
+     * @throws UserNotFoundException    if the authenticated user is not found
+     * @throws ArticleNotFoundException if the article is not found
+     */
+    Comment create(Principal principal, Long articleId, String commentBody);
 }
