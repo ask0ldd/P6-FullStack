43a7b0a9dc78f051702825e6e3f7b98ba9d9dff2
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-13 02:41:06 +0200

    *edit* back / adding new claims to the jwt

--- back/src/main/java/com/openclassrooms/mddapi/services/TokenService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/TokenService.java
@@ -1,13 +1,15 @@
 package com.openclassrooms.mddapi.services;
 
+import com.openclassrooms.mddapi.exceptions.UserNotFoundException;
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.repositories.UserRepository;
 import com.openclassrooms.mddapi.services.interfaces.ITokenService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.GrantedAuthority;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -17,9 +19,11 @@ import java.util.stream.Collectors;
 public class TokenService implements ITokenService {
 
     private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
 
-    public TokenService(JwtEncoder jwtEncoder) {
+    public TokenService(JwtEncoder jwtEncoder, UserRepository userRepository) {
         this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
     }
 
     public String generateJwt(Authentication authentication) {
@@ -30,7 +34,7 @@ public class TokenService implements ITokenService {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(" "));
 
-        // ((UserDetails) authentication.getDetails()).
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new UserNotFoundException("User not found."));
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
@@ -39,6 +43,8 @@ public class TokenService implements ITokenService {
                 .expiresAt(now.plus(1, ChronoUnit.DAYS))
                 .subject(authentication.getName())
                 .claim("roles", scope)
+                // add the id as a claim to the jwt for front-end filtering purposes
+                .claim("id", user.getId())
                 .build();
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
