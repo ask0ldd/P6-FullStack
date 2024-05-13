0970a85a3048a842f79b8938f6b2b8474ad3e3c4
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-13 02:36:29 +0200

    *edit* front / article list ordering fn

--- front/src/app/pages/articles/list/list.component.ts
+++ front/src/app/pages/articles/list/list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, of, take } from 'rxjs';
 import { IArticle } from 'src/app/interfaces/IArticle';
 import { ArticleService } from 'src/app/services/article.service';
 
@@ -8,14 +8,30 @@ import { ArticleService } from 'src/app/services/article.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
 
-  articles$ : Observable<IArticle[]> = this.articleService.all$()
+  orderAsc = false
+
+  articles!: IArticle[]
+  subscription! : Subscription
 
   constructor(private articleService : ArticleService) { }
 
   ngOnInit(): void {
-    // this.articles$ = this.articleService.all$()
+    const order = this.orderAsc ? "asc" : "desc"
+    if(this.subscription) this.subscription.unsubscribe()
+    this.subscription = this.articleService.allByOrder$(order).pipe(take(1)).subscribe(datas => this.articles = datas)
+  }
+
+  switchOrder(){
+    this.orderAsc = !this.orderAsc
+    const order = this.orderAsc ? "asc" : "desc"
+    if(this.subscription) this.subscription.unsubscribe()
+    this.subscription = this.articleService.allByOrder$(order).pipe(take(1)).subscribe(datas => this.articles = datas)
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) this.subscription.unsubscribe()
   }
 
 }
