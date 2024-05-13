6d6c1fee13e6b86fc7d60f084a3e2f15d9bec850
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-13 02:41:52 +0200

    *edit* front / article service : methods retrieving sorted articles

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -22,6 +22,10 @@ export class ArticleService {
     return this.http.get<IArticle[]>('api/articles')
   }
 
+  allByOrder$(order : string): Observable<IArticle[]>{
+    return this.http.get<IArticle[]>('api/articles/' + order)
+  }
+
   create$(newArticle : INewArticleRequest) : Observable<any>{
     return this.http.post<any>('api/article', newArticle)
   }
