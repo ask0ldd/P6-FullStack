7c39e478f63daae61d12f785c882c0514fde1a2b
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-05-21 18:10:32 +0200

    *edit* front / app component : conditional display for the header

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,35 +1,45 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
-  @ViewChild('sidenav') sidenav!: MatSidenav;
+export class AppComponent implements OnDestroy {
+  
+  title = 'front'
+  routerSub!: Subscription
 
-  showHeader = true;
-  showNav = true;
+  @ViewChild('sidenav') sidenav!: MatSidenav
+
+  showHeader = true
+  showNav = true
+  isProfileActive = false
 
   constructor(private router: Router) {
-    this.router.events.subscribe(event => {
+    this.routerSub = this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.handleNavigation(event.urlAfterRedirects);
+        this.handleNavigation(event.urlAfterRedirects)
       }
-    });
+    })
   }
 
   handleNavigation(url: string) {
     // header not needed on the homepage
-    const showHeaderUrls = ['/'];
-    this.showHeader = !showHeaderUrls.some(urlPath => url.includes(urlPath) && url.length === 1);
+    if(url == "/") this.showHeader = false
 
-    // url where the header is needed but not the nav
-    const showNavUrls = ['/login', '/register'];
-    this.showNav = !showNavUrls.some(urlPath => url.includes(urlPath));
+    // url where the nav part of the header isn't needed
+    const showNavUrls = ['/login', '/register']
+    this.showNav = !showNavUrls.some(urlPath => url.includes(urlPath))
+    if(showNavUrls.some(urlPath => url.includes(urlPath))){
+      const windowWidth = window.innerWidth
+      this.showHeader = windowWidth < 600 ? false : true
+    }
+
+    this.isProfileActive = url == "/profile" ? true : false
   }
 
   openMenu() : void {
@@ -38,5 +48,17 @@ export class AppComponent {
 
   closeMenu() : void {
     this.sidenav.close()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    const windowWidth = window.innerWidth
+    if(this.showNav === false && windowWidth < 600) this.showHeader = false
+    if(this.showNav === false && windowWidth >= 600) this.showHeader = true
+    if(windowWidth < 500) this.closeMenu()
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub.unsubscribe()
   }
 }
