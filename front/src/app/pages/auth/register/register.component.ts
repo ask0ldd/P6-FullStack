6565a6ad1e516e9daa7d01613abdae2c3b66e203
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-06-02 02:23:46 +0200

    *edit* front / register : making use of the storageService

--- front/src/app/pages/auth/register/register.component.ts
+++ front/src/app/pages/auth/register/register.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { take } from 'rxjs';
+import { Subscription, take } from 'rxjs';
 import { IRegisterRequest } from 'src/app/interfaces/Requests/IRegisterRequest';
 import { IJwtResponse } from 'src/app/interfaces/Responses/IJwtResponse';
 import { AuthService } from 'src/app/services/auth.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnDestroy {
 
   errorMessage = ""
+  subscription : Subscription | null = null
 
   public registerForm : FormGroup = this.fb.group({
     username : [
@@ -43,24 +45,28 @@ export class RegisterComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService) {
-  }
-
-  ngOnInit(): void {
-  }
+    private authService: AuthService,
+    private storageService : StorageService
+  ) { }
 
   onSubmit(): void{
     const registerRequest = this.registerForm.value as IRegisterRequest;
-    this.authService.register$(registerRequest).pipe(take(1)).subscribe({
+    this.subscription = this.authService.register$(registerRequest).pipe(take(1)).subscribe({
       next : (jwtResponse : IJwtResponse ) => {
-        console.log(jwtResponse)
-        this.authService.flushStorage()
-        localStorage.setItem('token', jwtResponse.token);
-        localStorage.setItem('username', jwtResponse.username)
+        this.storageService.flush()
+        this.storageService.setUserCredentials({username : jwtResponse.username, token : jwtResponse.token})
         this.router.navigateByUrl('/articles/list')
       },
-      error : (error : any) => { this.errorMessage = error?.error?.message}
+      error : (error : any) => { 
+        this.errorMessage = error.status === 401? "Cet email ne peut être utilisé." : error?.error?.message
+      }
     })
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription.unsubscribe()
+    }
   }
 
 }
